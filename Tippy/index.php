d77d597e52eebb8deabfbb8278f1a83c49a18249
Tianhao (Leo) Qiu <tiq001@ucsd.edu>
2016-12-12 00:39:42 -0800

    added the form to take in information

--- Tippy/index.php
+++ Tippy/index.php
@@ -1,7 +1,30 @@
 <!DOCTYPE HTML>  
 <html>
-<head>
-	<title> Tippy </title>
-	<meta charset="utf-8" />
-</head>
+	<head>
+		<title> Tippy </title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<h2>Tippy the Tip Calculator</h2>
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  
+		  Bill Subtotal: $<input type="text" name="subtotal" value="<?php echo $subtotal;?>">
+		  <span class="error"><?php echo $subtotalError;?></span>
+		  <br><br>
+		  Tip Percent:
+		  <br><br>
+		  <?php
+			//if the tip percent is not currently empty
+			if(!empty($_POST["tipPercent"])) $prevTip = $_POST["tipPercent"]*100;
+		    	else $prevTip = 15; //set default value to be 15%
+		    	$tips = [10, 15, 20]; //array of tip values
+			//for each value of the array, output the coorsponding redio button
+		    	foreach($tips as &$tipPercent)
+				printf("<input type=\"radio\" name=\"tipPercent\" value=\"%0.2f\" %s /> %d%% \t", 
+			    	$tipPercent/100,(intval($prevTip)==intval($tipPercent)) ? "checked" : "", $tipPercent);
+		  ?>
+		  <br><br>
+		  <input type="submit" name="submit" value="Submit">  
+		</form>
+	</body>
 </html>
